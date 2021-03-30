127c5b0ed167a828c5309e4004cb027126daf9d8
Ichika <ichiromand@yandex.ru>
2021-03-30 16:29:56 +0300

    ...

---
+++ script.js
@@ -0,0 +1,34 @@
+let list = document.querySelector('.list');
+let fieldInput = document.querySelector('.field__input');
+let field = document.querySelector('.field');
+
+fieldInput.onclick = function(){
+    if(!fieldInput.classList.contains('on')){
+        let buttons = document.createElement('div');
+        buttons.className = 'field__buttons';
+        field.appendChild(buttons);
+        let buttonCancel = document.createElement('button');
+        buttonCancel.className = 'field__buttons--cancel';
+        buttons.appendChild(buttonCancel);
+        buttonCancel.textContent = 'Отменить';
+        let buttonAdd = document.createElement('button');
+        buttonAdd.className = 'field__buttons--add';
+        buttons.appendChild(buttonAdd);
+        buttonAdd.textContent = 'Добавить';
+        fieldInput.classList.add('on')
+        
+        buttonAdd.addEventListener('click', function(){
+            let listTag = document.createElement('li');
+            listTag.className = 'list__tag';
+            list.appendChild(listTag);
+            listTag.textContent = fieldInput.value;
+            fieldInput.value = '';
+        })
+        
+        buttonCancel.addEventListener('click', function(){
+            buttons.remove();
+            fieldInput.value = '';
+            fieldInput.classList.remove('on')
+        })
+    }
+}
